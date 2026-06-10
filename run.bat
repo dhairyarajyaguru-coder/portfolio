535ebaa60cdff2af4d4e6a8f096c6a89f05e2bba
Dhairya Rajyayguru <232301708+dhairyarajyaguru-coder@users.noreply.github.com>
2026-06-10 19:20:52 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title Open Dhairya Rajyaguru Portfolio
+echo ==========================================================
+echo Opening your portfolio...
+echo ==========================================================
+echo.
+start index.html
+exit
